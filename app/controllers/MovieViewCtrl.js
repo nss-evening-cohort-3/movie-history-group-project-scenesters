505928c4231d858b51c1d8b809f2c4b4ccdaa8de
scottpschaffer <scottpschaffer@yahoo.com>
2016-06-03 19:54:11 -0500

    putting info on partial

--- app/controllers/MovieViewCtrl.js
+++ app/controllers/MovieViewCtrl.js
@@ -5,7 +5,7 @@ app.controller("MovieViewCtrl", function($scope, $routeParams, movieStorage){
   movieStorage.getMovieList().then(function(movieCollection){
     console.log("movieCollection from promise", movieCollection);
     $scope.movies = movieCollection;
-
+    console.log("First:", $scope.movies[0][1].Title);
     $scope.selectedMovie = $scope.movies.filter(function(movie){
       return movie.id === $routeParams.itemId;
     })[0];
