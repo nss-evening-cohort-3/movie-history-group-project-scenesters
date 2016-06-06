4c0bfbf9f73fde991c2630a795c718dbbfe34b70
jkillz2020 <kilgore.josh@gmail.com>
2016-06-06 08:08:01 -0500

    add script references for login ctrl plus minor fixes

--- app/Controllers/LoginCtrl.js
+++ app/Controllers/LoginCtrl.js
@@ -37,7 +37,7 @@ app.controller("LoginCtrl", function($scope, $rootScope, $location, firebaseURL,
     AuthFactory
       .authenticate($scope.account)
       .then(() => {
-        // $scope.hasUser = true;
+        $scope.hasUser = true;
         $rootScope.isActive = true;
         $location.path("/");
         $scope.$apply();
--- app/app.js
+++ app/app.js
@@ -2,27 +2,27 @@ var app = angular.module("MovieHistoryApp", ["ngRoute"])
   .constant("firebaseURL","https://movie-scenesters.firebaseio.com/")
   .constant("omdbURL", "http://www.omdbapi.com/");
 
-// let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
-//   if(AuthFactory.isAuthenticated()){
-//     console.log("User is authenticated, resolve route promise");
-//     resolve();
-//   } else {
-//     console.log("User is not authenticated, reject route promise");
-//     reject();
-//   }
-// })
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+  if(AuthFactory.isAuthenticated()){
+    console.log("User is authenticated, resolve route promise");
+    resolve();
+  } else {
+    console.log("User is not authenticated, reject route promise");
+    reject();
+  }
+})
 
 app.config(function($routeProvider){
   $routeProvider.
     when('/',{
       templateUrl: 'partials/movie-list.html',
       controller: 'MovieListCtrl',
-      // resolve: {isAuth}
+       resolve: {isAuth}
       }).
     when('/movies/list',{
       templateUrl: 'partials/movie-list.html',
       controller: 'MovieListCtrl',
-      // resolve: {isAuth}
+       resolve: {isAuth}
       }).
       when('/logout', {
         templateUrl: 'partials/login.html',
@@ -31,15 +31,15 @@ app.config(function($routeProvider){
       otherwise('/');
 });
 
-// app.run(($location) =>{
-//   let todoRef = new Firebase("https://movie-scenesters.firebaseio.com/");
+app.run(($location) =>{
+  let todoRef = new Firebase("https://movie-scenesters.firebaseio.com/");
 
-//   todoRef.onAuth(authData =>{
-//     if(!authData){
-//       $location.path("/login");
-//     }
-//   })
-// })
+  todoRef.onAuth(authData =>{
+    if(!authData){
+      $location.path("/login");
+    }
+  })
+})
 
 
 
